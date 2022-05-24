0497500581ec5d6566c7b9e2f2af4168e8d76beb
Alexander Rathke Johansen <alerj09@gmail.com>
2022-05-24 14:43:04 +0200

    Fixed batch file so that images are built correctly and containers are run properly. Still need to make data in database persistent.

---
+++ refresh_containers_and_run.bat
@@ -0,0 +1,18 @@
+docker stop wishlist-node
+docker stop wishlist-db-postgres-new
+
+docker container rm wishlist-node
+docker container rm wishlist-db-postgres-new
+
+cd ./server/
+docker build --rm --no-cache -t wishlist-app-node-backend ./
+cd ../database/
+docker build --rm --no-cache -t wishlist-postgres-db
+
+cd ..
+docker run -d --name wishlist-db-postgres-new -p 5432:5432 --network wishlist-net wishlist-postgres-db
+docker run -d --name wishlist-node -p 3001:3001 --network wishlist-net wishlist-app-node-backend
+
+set /p DUMMY=Stop containers by pressing enter...
+docker stop wishlist-node
+docker stop wishlist-db-postgres-new
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Prisma, User } from "@prisma/client";
 import e from "express";
+import { env } from "process";
 
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
@@ -14,16 +15,18 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(session({
-  secret: process.env.SESSION_SECRET || "645hfgj-&RYUfhgh8w3r",
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false //Needs to be changed once https has been put in place
-  }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "645hfgj-&RYUfhgh8w3r",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, //Needs to be changed once https has been put in place
+    },
+  })
+);
 
 const addWishlist = async () => {};
 
@@ -37,16 +40,16 @@ const addUser = async (user: Prisma.UserCreateInput) => {
 const getUsers = async () => {
   const users = await prisma.user.findMany();
   return users;
-}
+};
 
 app.get("/api", (req, res) => {
-  console.log("Request received!");
+  console.log("Request received! DB ULR: " + process.env.DATABASE_URL);
 
   const session = req.session;
   if (session) {
     res.json({
       message: session.loggedin ? "Hello, " + session.username + "!" : "Hello!",
-      loggedin: session.loggedin
+      loggedin: session.loggedin,
     });
   } else {
     res.status(500).json("An error occurred: Could not load session data.");
@@ -54,36 +57,46 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-    getUsers().then((users) => {
+  console.log("GETTING USERS, DB_URL: " + process.env.DATABASE_URL);
+  getUsers()
+    .then((users) => {
       res.json(users);
-    }).catch((e) => {
-      console.error(e.message);
-      res.status(500).json("An error occurred while getting users from database!");
     })
+    .catch((e) => {
+      console.error(e.message);
+      res
+        .status(500)
+        .json("An error occurred while getting users from database!");
+    });
 });
 
 app.post("/api/register", async (req, res) => {
   try {
-    const {username, password, firstName, lastName, dateOfBirth, email} = req.body;
+    const { username, password, firstName, lastName, dateOfBirth, email } =
+      req.body;
 
-    if (!(username && password && firstName && lastName && dateOfBirth && email)) {
+    if (
+      !(username && password && firstName && lastName && dateOfBirth && email)
+    ) {
       res.status(400);
       throw new Error("Missing input!");
     }
 
-    if(username.length < 1) {
+    if (username.length < 1) {
       res.status(400);
       throw new Error("Please enter a username!");
     }
 
-    if(password.length < 8) {
+    if (password.length < 8) {
       res.status(400);
       throw new Error("Password needs to be at least 8 characters long!");
     }
-    
-    if(illegalUsernameFormat.test(username)) {
+
+    if (illegalUsernameFormat.test(username)) {
       res.status(400);
-      throw new Error("Only letters a-z, A-Z and numbers are allowed in a username!");
+      throw new Error(
+        "Only letters a-z, A-Z and numbers are allowed in a username!"
+      );
     }
 
     let encryptedPassword;
@@ -94,16 +107,18 @@ app.post("/api/register", async (req, res) => {
       throw new Error("An error occurred while hashing information!");
     }
 
-    if(await prisma.user.findFirst({ where: {username: username} })) {
+    if (await prisma.user.findFirst({ where: { username: username } })) {
       res.status(409);
       throw new Error("Username already in use!");
     }
-    
-    if(await prisma.user.findFirst({ where: {email: email} })) {
+
+    if (await prisma.user.findFirst({ where: { email: email } })) {
       res.status(409);
-      throw new Error("An account with that email already exists. Please log in instead.");
+      throw new Error(
+        "An account with that email already exists. Please log in instead."
+      );
     }
-    
+
     const newUser = {
       username: username,
       password: encryptedPassword,
@@ -116,56 +131,67 @@ app.post("/api/register", async (req, res) => {
     if (!addUser(newUser)) {
       res.status(500);
       throw new Error("Could not add user!");
-    };
+    }
     res.status(200).json({
       authToken: "test",
-      message: "User account created!"
+      message: "User account created!",
     });
-  } catch(e: any) {
-    let message = "Could not register due to an unknown internal server error. Please try again later.";
+  } catch (e: any) {
+    let message =
+      "Could not register due to an unknown internal server error. Please try again later.";
     if (e instanceof Error) message = e.message;
     res.json(message);
-  };
+  }
 });
 
 app.post("/api/login", async (req, res) => {
   try {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
     console.log(req.body);
     const session = req.session;
-    
+
     if ((!email && !username) || !password) {
       res.status(400).json("Input missing!");
     }
 
     let targetUser: User | undefined;
     if (email) {
-      targetUser = await prisma.user.findFirst({where: {email: email}});
+      targetUser = await prisma.user.findFirst({ where: { email: email } });
     } else if (username) {
-      targetUser = await prisma.user.findFirst({where: {username: username}});
+      targetUser = await prisma.user.findFirst({
+        where: { username: username },
+      });
     }
 
     if (targetUser) {
-      await bcrypt.compare(password, targetUser!.password, (_err: any, approved: boolean) => {
-        if (approved) {
-          if (session) {
-            session.userId = targetUser!.id;
-            session.username = targetUser!.username;
-            session.loggedin = true;                                                                    //NEEDS TO BE LOOKED INTO, IS THIS SECURE?? MAYBE USE SessionId?
+      await bcrypt.compare(
+        password,
+        targetUser!.password,
+        (_err: any, approved: boolean) => {
+          if (approved) {
+            if (session) {
+              session.userId = targetUser!.userId;
+              session.username = targetUser!.username;
+              session.loggedin = true; //NEEDS TO BE LOOKED INTO, IS THIS SECURE?? MAYBE USE SessionId?
+            }
+            res.status(200).json({
+              message: `Login successful! Welcome, ${targetUser?.firstName}.`,
+              loggedin: true,
+            });
+          } else {
+            res
+              .status(401)
+              .json(
+                "Login failed. Please check that your login information is correct."
+              );
           }
-          res.status(200).json({
-            message: `Login successful! Welcome, ${targetUser?.firstName}.`,
-            loggedin: true,
-          });
-        } else {
-          res.status(401).json("Login failed. Please check that your login information is correct.");
         }
-      })
+      );
     } else {
       res.status(401).json("Login failed. User not found!");
     }
   } catch {
-    console.log("An error occurred while trying to log in!")
+    console.log("An error occurred while trying to log in!");
   }
 });
 
