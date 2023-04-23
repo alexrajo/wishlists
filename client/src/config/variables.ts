da37b04187b970c1a3c10c81557ecdfe96a2f698
Alexander Rathke Johansen <alerj09@gmail.com>
2023-04-23 21:24:07 +0200

    chore: Fixed development API host IP-address

--- client/src/config/variables.ts
+++ client/src/config/variables.ts
@@ -1,2 +1,4 @@
 export const HOST =
-  process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://wishlister.azurewebsites.net";
+  process.env.NODE_ENV === "development"
+    ? "http://10.0.2.2:3001"
+    : "https://wishlister.azurewebsites.net";
