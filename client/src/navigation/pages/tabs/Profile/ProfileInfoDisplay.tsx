c7e128791eaee20da7745b890387219fe263ef49
Alexander Rathke Johansen <alerj09@gmail.com>
2023-04-23 22:13:59 +0200

    style: Improved responsivity for profile info page

--- client/src/navigation/pages/tabs/Profile/ProfileInfoDisplay.tsx
+++ client/src/navigation/pages/tabs/Profile/ProfileInfoDisplay.tsx
@@ -1,4 +1,14 @@
-import { Avatar, View, Center, Box, Heading, Text, VStack, Button } from "native-base";
+import {
+  Avatar,
+  View,
+  Center,
+  Box,
+  Heading,
+  Text,
+  VStack,
+  Button,
+  Flex,
+} from "native-base";
 import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
 import { Pressable, StyleSheet } from "react-native";
 import useAuth from "../../../../hooks/useAuth";
@@ -16,7 +26,12 @@ const ProfileInfoDisplay = ({ navigation }: any) => {
   });
 
   const [request, setRequest] = useState(createNewRequest());
-  const { data: profileData, error: fetchError, isPending, refresh } = useFetch<SignUpData>(request);
+  const {
+    data: profileData,
+    error: fetchError,
+    isPending,
+    refresh,
+  } = useFetch<SignUpData>(request);
 
   const onSettingsButtonPressed = () => {
     navigation.navigate("Settings", profileData);
@@ -28,7 +43,7 @@ const ProfileInfoDisplay = ({ navigation }: any) => {
 
   return (
     <View flex={1}>
-      <Center flex={1}>
+      <Flex flex={1}>
         {fetchError && (
           <>
             <Text color={"red.500"}>{fetchError}</Text>
@@ -36,50 +51,66 @@ const ProfileInfoDisplay = ({ navigation }: any) => {
           </>
         )}
         {profileData && (
-          <Box rounded="md" style={styles.userInfoContainer}>
-            <Pressable style={styles.openSettingsButton} onPress={onSettingsButtonPressed}>
-              <FontAwesome5 name="cog" size={20} color="gray" />
-            </Pressable>
-            <VStack space={5} style={styles.userInfoInnerContainer}>
-              <Avatar bg="blue.300" size="xl" style={styles.profilePicture}>
-                {DefaultAvatarIcon()}
-              </Avatar>
-              <Box alignItems={"center"}>
-                <Heading textAlign="center">{profileData.firstName + " " + profileData.lastName}</Heading>
-                <Text fontStyle={"italic"}>@{profileData.username}</Text>
-              </Box>
-              <Text>{profileData.email}</Text>
-              <Box>
-                <Text alignSelf="flex-start" fontWeight="semibold">
-                  About me:
-                </Text>
-                <Text marginX={3} fontStyle="italic">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi finibus tempor urna pretium convallis.
-                  Sed ut ipsum ornare lorem eleifend sollicitudin.
-                </Text>
-              </Box>
-            </VStack>
-          </Box>
+          <View style={styles.userInfoContainer}>
+            <Box flex={1} bg={"white"} rounded={"md"} marginTop={50}>
+              <Pressable
+                style={styles.openSettingsButton}
+                onPress={onSettingsButtonPressed}
+              >
+                <FontAwesome5 name="cog" size={20} color="gray" />
+              </Pressable>
+              <VStack space={5} style={styles.userInfoInnerContainer}>
+                <Avatar bg="blue.300" size="xl" style={styles.profilePicture}>
+                  {DefaultAvatarIcon()}
+                </Avatar>
+                <Box alignItems={"center"}>
+                  <Heading textAlign="center">
+                    {profileData.firstName + " " + profileData.lastName}
+                  </Heading>
+                  <Text fontStyle={"italic"}>@{profileData.username}</Text>
+                </Box>
+                <Text>{profileData.email}</Text>
+                <Box>
+                  <Text alignSelf="flex-start" fontWeight="semibold">
+                    About me:
+                  </Text>
+                  <Text numberOfLines={4} marginX={3} fontStyle="italic">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi finibus tempor urna pretium convallis. Sed ut ipsum
+                    ornare lorem eleifend sollicitudin.
+                  </Text>
+                </Box>
+              </VStack>
+            </Box>
+          </View>
         )}
-      </Center>
+      </Flex>
     </View>
   );
 };
 
-const DefaultAvatarIcon = () => <MaterialCommunityIcons name="account" size={80} color="white" />;
+const DefaultAvatarIcon = () => (
+  <MaterialCommunityIcons name="account" size={80} color="white" />
+);
 
 const styles = StyleSheet.create({
   userInfoContainer: {
-    backgroundColor: "white",
-    alignSelf: "stretch",
-    margin: 30,
+    position: "absolute",
+    flex: 0,
+    top: 30,
+    bottom: 30,
+    left: 30,
+    right: 30,
   },
 
   userInfoInnerContainer: {
     paddingHorizontal: 25,
     alignItems: "center",
-    transform: [{ translateY: -50 }],
     zIndex: 0,
+    position: "absolute",
+    bottom: 5,
+    top: -50,
+    overflow: "hidden",
   },
 
   profilePicture: {
